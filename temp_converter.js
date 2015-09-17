57eb5f9cc6214948c8f6035d7c6246e5149f6a13
Marques DeHerrera <session1@START-Coding-MBP-C02PP75TG8WN.local>
2015-09-17 14:33:58 -0700

    I added the Arrays for the temperatures for each of the temperatures

--- temp_converter.js
+++ temp_converter.js
@@ -8,21 +8,27 @@ var fahrenheitToKelvin = (fahrenheit +459.67) / 1.8;
 // Enter console.log statements below...
 console.log("Starting Temperature:"+ fahrenheit + "F");
 console.log("Converted To Celsius:"+ fahrenheitToCelsius + "C");
-console.log("Converted To Kelvin:"+ fahrenheitToKelvin + "K")
+console.log("Converted To Kelvin:"+ fahrenheitToKelvin + "K");
 // Repeat the above for Celsius and Kelvin...
 var celsius = 0;
-var celsiustofahrenheit = (celsius * 1.8+32)
-var celsiustokelvin = (celsius + 273.15)
-console.log("Starting Temperature:"+ celsius + "C")
-console.log("Converted To fahrenheit:"+ celsiustofahrenheit + "F")
-console.log("Converted to kelvin:" + celsiustokelvin + "K")
+var celsiustofahrenheit = (celsius * 1.8+32);
+var celsiustokelvin = (celsius + 273.15);
+console.log("Starting Temperature:"+ celsius + "C");
+console.log("Converted To fahrenheit:"+ celsiustofahrenheit + "F");
+console.log("Converted to kelvin:" + celsiustokelvin + "K");
 
-var kelvin = 273.15;
-var kelvintofahrenheit = (kelvin * 1.8 - 459.67)
-var kelvintocelsius = (kelvin - 273.15)
-console.log("Starting Temperature"+ celsius + "C")
-console.log("Converted To fahrenheit:"+ kelvintofahrenheit + "F")
-console.log("converted To Celsius:" + kelvintocelsius + "C")
+var kelvin = 273.15
+var kelvintofahrenheit = (kelvin * 1.8 - 459.67);
+var kelvintocelsius = (kelvin - 273.15);
+console.log("Starting Temperature"+ celsius + "C");
+console.log("Converted To fahrenheit:"+ kelvintofahrenheit + "F");
+console.log("converted To Celsius:" + kelvintocelsius + "C");
 
+
+var fahrenheitTemps = [ fahrenheit, fahrenheitToCelsius, fahrenheitToKelvin ];
+
+var celsiusTemps = [ celsius, celsiustofahrenheit, celsiustokelvin];
+
+var kelvinTemps = [kelvin, kelvintofahrenheit, kelvintocelsius];
 
 '$ node temp_converter.js'
